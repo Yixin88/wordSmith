4f275885d82c626b54fe0f6bcc52d4c92921580c
Yixin88 <yixin88@hotmail.co.uk>
2022-11-12 23:13:56 +0000

    added function to change the color of button depending on formatting to show its on/off, also remove format also reset style

--- script.js
+++ script.js
@@ -7,6 +7,14 @@ function checkEmpty() {
     return true;
 }
 
+function checkToggle(className, childrenName) {
+    if (el.classList.contains(className)) {
+        document.querySelector(".button_Container").children[childrenName].style.backgroundColor = "darkBlue";
+    } else {
+        document.querySelector(".button_Container").children[childrenName].style.backgroundColor = "#AA96DA";
+    }
+}
+
 function setColor() {
     const input = document.querySelectorAll("input")[0];
     el.style.color = input.value;
@@ -15,30 +23,37 @@ function setColor() {
 function toggleUnderline() {
     if (checkEmpty()) {
         el.classList.toggle("underline");
+        checkToggle("underline", 0);
     }
 }
 
 function toggleItalic() {
     if (checkEmpty()) {
         el.classList.toggle("italic");
+        checkToggle("italic", 1);
     }
 }
 
 function toggleBold() {
     if (checkEmpty()) {
         el.classList.toggle("bold");
+        checkToggle("bold", 2);
     }
 }
 
 function toggleMonospace() {
     if (checkEmpty()) {
         el.classList.toggle("mono");
+        checkToggle("mono", 3);
     }
 }
 
 function removeFormat() {
     el.classList.remove("underline", "italic", "bold", "mono");
     el.style.color = "black";
+    document.querySelectorAll(".button_Container button").forEach(btn => {
+        btn.style.backgroundColor = "#AA96DA";
+    })
 }
 
 function clearInput() {
